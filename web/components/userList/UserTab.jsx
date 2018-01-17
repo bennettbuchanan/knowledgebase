0e593ce2942fead1a01fab1b551bbbd5bf05712f
Bennett Buchanan <bennett.buchanan@scality.com>
2018-01-17 13:13:01 -0800

    Make user listing public

--- web/components/userList/UserTab.jsx
+++ web/components/userList/UserTab.jsx
@@ -6,14 +6,32 @@ import { Row, Col, Tab, Alert } from 'react-bootstrap/lib';
  * The listing for a group of users in a tab.
  */
 class UserTab extends Component {
-
-    getUsers = (users) => {
-        if (users.length === 0) {
-            return (
+    getAlert = (isUserSignedIn) => {
+        const signInButton = this.props.title === 'Learn' ?
+            'kb-signin-large-1' : 'kb-signin-large-2';
+        const message = isUserSignedIn ? 'No matches were found.' :
+            'Please sign in to find matches.'
+        return (
+            <div>
                 <Alert bsStyle="info" style={{ margin: '0px 15px' }}>
-                    No matches were found.
+                    {message}
+                    {!isUserSignedIn &&
+                        <div style={{ marginTop: '15px' }}
+                            id={signInButton}
+                        />
+                    }
                 </Alert>
-            )
+            </div>
+        )
+    }
+
+    getUsers = (users, isUserSignedIn) => {
+        // Show an alert if the user is not signed in but is viewing the Learn
+        // or Share tab, or if the user is signed in but there are no matches.
+        const showAlert = this.props.title !== 'All' &&
+            (!isUserSignedIn || users.length === 0);
+        if (showAlert) {
+            return (this.getAlert(isUserSignedIn))
         }
         return users.map((user, i) => (
             <Col sm={12} md={6} key={user.id}>
@@ -24,6 +42,9 @@ class UserTab extends Component {
                     name={`${user.first_name} ${user.last_name}`}
                     email={user.email}
                     image={user.image}
+                    isUserSignedIn={isUserSignedIn}
+                    renderButtons={this.props.renderButtons}
+                    loadGoogleAPI={this.props.loadGoogleAPI}
                     errorHandler={this.props.errorHandler}
                 />
             </Col>
@@ -31,12 +52,12 @@ class UserTab extends Component {
     }
 
     render() {
-        const { eventKey, title, heading, users } = this.props;
+        const { eventKey, title, heading, users, isUserSignedIn } = this.props;
         return (
             <Tab eventKey={eventKey} title={eventKey}>
                 <h1>{heading}</h1>
                 <Row className="show-grid">
-                    {this.getUsers(users)}
+                    {this.getUsers(users, isUserSignedIn)}
                 </Row>
             </Tab>
         );
